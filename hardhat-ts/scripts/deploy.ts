aa28bb4402a789c614e5747d075fe3d2767c5557
David Hurley <dhurls99@gmail.com>
2023-03-29 09:39:10 -0400

    deploy bug

--- hardhat-ts/scripts/deploy.ts
+++ hardhat-ts/scripts/deploy.ts
@@ -1,14 +1,14 @@
-// import '@nomiclabs/hardhat-waffle';
+import '@nomiclabs/hardhat-waffle';
 require('@nomiclabs/hardhat-ethers');
-import { ethers } from 'hardhat';
+// import { ethers } from 'hardhat';
 
 import { allowlist } from './allowlist';
 
 async function main() {
-  const NFT = await ethers.getContractFactory('Finale');
-  const nft = await NFT.deploy(allowlist);
-  await nft.deployed();
-  console.log('NFT Contract Deployed at ' + nft.address);
+  // const NFT = await ethers.getContractFactory('Finale');
+  // const nft = await NFT.deploy(allowlist);
+  // await nft.deployed();
+  // console.log('NFT Contract Deployed at ' + nft.address);
 }
 
 main().catch((error) => {
